98d747a9b9188d92828f936a0688b79927c17171
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2023-12-18 12:45:36 -0500

    fixed inquirer require() compatibility

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const inquirer = require("inquirer");
+const jest = require("jest");
+
+//const fs = require('fs');
+//const inquirer = require('inquirer');
+//const genMd = require('./utils/generateMarkdown.js');
+inquirer
+  .prompt ([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'What shape do you want?',
+      choices: ['Circle', 'Square', 'Triangle']
+    },
+    {
+      type: 'input',
+      name: 'initials',
+      message: 'Give some letters to add to the logo (up to 3)'
+    },
+    {
+      type: 'input',
+      name: 'background-color',
+      message: 'What color do you want the shape to be?'
+    },
+    {
+      type: 'input',
+      name: 'text-color',
+      message: 'What color do you want your text to be?'
+    }
+  ])
+  .then ((response) => {
+    console.log(response)
+  })
